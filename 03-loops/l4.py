c9a2d5476c7be085f804f1ce1acc4321c7e0ba54
Hugh Lilly <hughlilly@users.noreply.github.com>
2023-03-27 10:56:16 +1300

    loops: break enumeration out into var

--- 03-loops/l4.py
+++ 03-loops/l4.py
@@ -10,7 +10,8 @@ print("Itemised shopping list:")
 
 # Pass the list object to an iterator function, enumerate()
 # This allows us to access each item's position in the list
-for position, item in enumerate(shopping_list):
+enumerated_list = enumerate(shopping_list)
+for position, item in enumerated_list:
     print(f"{position}. {item}")
 
 # So you can loop over objects, and you can loop over functions/iterators
